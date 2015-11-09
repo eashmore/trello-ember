53c7fcddd6fba06a04a2985f3fdd77e47dc797f9
Eric Ashmore <edashmore@gmail.com>
2015-11-08 19:11:22 -0800

    remove ember-cli bootstrap

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,16 +3,16 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
-var App;
+var TrelloClone;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-App = Ember.Application.extend({
+TrelloClone = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(TrelloClone, config.modulePrefix);
 
-export default App;
+export default TrelloClone;
